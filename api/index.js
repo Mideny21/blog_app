205c114dc8a31fbcc1a28c83a8cb22fde36a65df
Mideny21 <30451596+Mideny21@users.noreply.github.com>
2021-07-07 17:12:30 +0300

    file uploads

--- api/index.js
+++ api/index.js
@@ -2,10 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const categoryRoute = require("./routes/categories");
-const multer = require("multer");
 
 const app = express();
 
@@ -29,24 +25,7 @@ mongoose
     console.log("DB connection succesfully");
   });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, "hello.jpeg");
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
-
 app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running");
