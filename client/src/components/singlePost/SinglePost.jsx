66494ad7afe2b83b533d71d7ce8333cf70ebb772
Mideny21 <30451596+Mideny21@users.noreply.github.com>
2021-07-11 13:45:54 +0300

    mern updates

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,12 +1,30 @@
+import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 import "./SinglePost.css";
+import axios from "axios";
 
 export default function SinglePost() {
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data);
+    };
+    getPost();
+  }, [path]);
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img src="" className="singlePostImg" alt="" />
+        {post.photo && (
+          <img src={post.photo} className="singlePostImg" alt="" />
+        )}
         <h1 className="singlePostTitle">
-          Lorem ipsum
+          {post.title}
           <div className="singlePostEdit">
             <i class="singlePostIcon far fa-edit"></i>
             <i class="singlePostIcon far fa-trash-alt"></i>
@@ -14,11 +32,16 @@ export default function SinglePost() {
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
-            Author: <b>Safak</b>
+            Author:
+            <Link to={`/?user=${post.username}`} className="link">
+              <b>{post.username}</b>
+            </Link>
           </span>
-          <span className="singlePostDate">1 hour ago</span>
+          <span className="singlePostDate">
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-        <p className="singlePostDesc">lorem ipsum</p>
+        <p className="singlePostDesc">{post.desc}</p>
       </div>
     </div>
   );
